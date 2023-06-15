bbea6920291a66c61fe653d4beed915ff6085f31
byxkbyxk <44155082+byxkbyxk@users.noreply.github.com>
2023-06-15 17:46:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/sirpdboy/luci-app-advanced.git ./package/lean/luci-app-advanced
-git clone https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/openwrt-passwall
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git ./package/luci-theme-infinityfreedom
 git clone https://github.com/chenhw2/luci-app-aliddns.git ./package/feeds/luci-app-aliddns
 #rm -rf ./package/lean/luci-app-mwan3helper
